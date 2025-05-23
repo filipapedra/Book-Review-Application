28cb22a518e1fafb660f153e05644674e25ee67a
filipapedra <filipapedra@gmail.com>
2025-05-23 08:30:35 -0400

    books tittle code changed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,14 +24,30 @@ public_users.get('/isbn/:isbn',function (req, res) {
   if (book) {
     return res.status(200).json(book);
   } else {
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(404).json({message: "book not foundd"});
   }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author.toLowerCase();
+  const booksByAuthor = [];
+
+  // Get all ISBN keys from the books object
+  const bookKeys = Object.keys(books);
+
+  // Loop through all books and find matches by author
+  bookKeys.forEach((isbn) => {
+    if (books[isbn].author.toLowerCase() === author) {
+      booksByAuthor.push(books[isbn]);
+    }
+  });
+
+  if (booksByAuthor.length > 0) {
+    return res.status(200).json(booksByAuthor);
+  } else {
+    return res.status(404).json({ message: "No books found by this author" });
+  }
 });
 
 // Get all books based on title

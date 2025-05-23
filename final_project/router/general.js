1965514f9e9b8191ba854ad5c637bfbe5c1c4711
filipapedra <filipapedra@gmail.com>
2025-05-23 08:35:44 -0400

    getting books reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -57,9 +57,17 @@ public_users.get('/title/:title',function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book && book.reviews) {
+    return res.status(200).json(book.reviews);
+  } else if (book && !book.reviews) {
+    return res.status(200).json({ message: "No reviews found for this book" });
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
